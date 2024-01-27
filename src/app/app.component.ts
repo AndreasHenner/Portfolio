16770a53480c763934cda0c9575dd5b21ff6c848
Andreas Henner <134337273+AndreasHenner@users.noreply.github.com>
2024-01-27 17:07:31 +0100

    translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { MainContentComponent } from './main-content/main-content.component';
 import { FooterComponent } from './footer/footer.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { HeaderComponent } from './main-content/header/header.component';
-
-
+import { TranslateService } from '@ngx-translate/core';
+import { HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -17,14 +17,21 @@ import { HeaderComponent } from './main-content/header/header.component';
     MainContentComponent,
     FooterComponent,
     ImprintComponent,
-    HeaderComponent
+    HeaderComponent,
+    HttpClientModule
   ],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 
+
 export class AppComponent  {
+
+  constructor(public translate: TranslateService) {
+    translate.use('en');
+  }
+
   title = 'portfolio';
   hideHeader = false;
   imprintClicked: boolean = false;
